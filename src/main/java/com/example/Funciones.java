05462eb52fca88f07b72cbe8336e91c2b3088849
daniela <moreno.tobar.daniela@gmail.com>
2023-02-02 18:24:05 -0500

    funciones

---
+++ src/main/java/com/example/Funciones.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Funciones {
+
+    public static void main(String[] args) {
+        saludo();
+        saludo("Luke");
+        saludo("Leia");
+        System.out.println(devolverSaludo());
+    }
+
+    //Agrupacion de sentencias que pueden ser reutilizadas
+    private static void saludo(){
+        System.out.println("Hello World");
+    }
+
+    private static void saludo(String name){
+        System.out.println("Hello World " + name);
+    }
+
+    private static String devolverSaludo(){
+        return "Hola";
+    }
+}

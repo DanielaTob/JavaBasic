63c292bbab02527cf7705abecbf80510101e7c30
daniela <moreno.tobar.daniela@gmail.com>
2023-02-02 17:30:18 -0500

    operadores

---
+++ src/main/java/com/example/Operadores.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        //Operadores
+
+        //Aritmeticos
+        int number1 = 1;
+        int number2 = 2;
+
+        int resultado1 = number1 + number2;
+        int resultado2 = number1 - number2;
+        int resultado3 = number2 * number1;
+        int resultado4 = number2 / number1;
+
+        //logicos, comparacion, relacion, booleanos, incremento, decremento, concatenacion
+    }
+}

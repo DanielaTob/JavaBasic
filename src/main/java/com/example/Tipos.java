ebbf0995921b7e90e426d8830deea4e62654e47f
daniela <moreno.tobar.daniela@gmail.com>
2023-02-02 15:02:18 -0500

    first commit

---
+++ src/main/java/com/example/Tipos.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        //tipo - identificador - = 30
+        //Enteros
+        byte number1 = 1;   //1 byte
+        short number2 = 2;  //2 byte
+        int number3 = 3;    //4 byte
+        long number4 = 4;   //8 byte
+
+        //punto flotante
+        float dcimal = 4.9f;
+        double decimal2 = 9.99d;
+
+        //caracter
+        char caracter1 = 'a';
+
+        //booleanos
+        boolean active1 = true;
+        boolean active2 = false;
+
+        //cadenas de texto
+        String nombre = "luke";
+        String apellido = "Skywalker";
+
+        //tipos envoltorio
+        Integer numero = null;
+        Long numero2 = 2L;
+
+    }
+}

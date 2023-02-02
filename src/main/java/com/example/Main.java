e60fc4bdae7824003d32bbe8c1fc7aa407c97e0e
daniela <moreno.tobar.daniela@gmail.com>
2023-02-02 18:46:07 -0500

    ejercicio tema 1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the way!");
+
+        //Ejercicio tema 1
+
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+        float decimal = 4.9f;
+        double decimal2 = 9.99d;
+        char character1 = 'a';
+        boolean active1 = true;
+        boolean active2 = false;
+        String name = "luke";
+        String lastname = "Skywalker";
+        Integer number = null;
+
     }
 }

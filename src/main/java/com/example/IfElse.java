aa5ef5134d69f089b0863061c011b154385c1425
daniela <moreno.tobar.daniela@gmail.com>
2023-02-02 19:45:59 -0500

    bucles

---
+++ src/main/java/com/example/IfElse.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+        boolean check = 5 < 10;
+
+        int number1 = 50;
+        int number2 = 10;
+        int number3 = 20;
+        int number4 = 70;
+
+        if (number1 < number2 && number2 < number3){
+            System.out.println("Es correcto");
+        } else if (number3 < number4) {
+            System.out.println("Es correcto también");
+        } else{
+            System.out.println("Incorrecto");
+        }
+
+    }
+}
---
+++ src/main/java/com/example/While.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class While {
+
+    public static void main(String[] args) {
+
+        boolean check = true;
+        int count = 0;
+
+        while(count < 10){
+            System.out.println("Hola mundo");
+            count++; //condicion
+        }
+        System.out.println("fin...");
+    }
+}
